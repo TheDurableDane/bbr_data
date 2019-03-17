d7dd8d3f3100333c86086a16dfb161bad5acbab2
thomas <thomaslolkschmidt@gmail.com>
2019-03-17 20:28:36 +0100

    Backup of code.

--- scrape_data.py
+++ scrape_data.py
@@ -10,15 +10,10 @@ def get_zipcodes():
     zipcodes_dropdown = soup.find_all('select', class_='form-control border rounded p-2')[0].contents
 
     zipcodes = []
-    for item in zipcodes_dropdown:
+    for item in zipcodes_dropdown[1:]:
         if item != "\n":
-            zipcode_content = item.contents[0].split()[0]
-            try:
-                zipcode = int(zipcode_content)
-            except ValueError as e:
-                print("Item '{}' could not be converted to datatype integer.".format(zipcode_content))
-            else:
-                zipcodes.append(zipcode)
+            zipcode = item.contents[0].split()[0]
+            zipcodes.append(zipcode)
 
     return zipcodes
 
@@ -33,4 +28,23 @@ def get_streetnames(zipcode):
 
 
 zipcodes = get_zipcodes()
-streetnames = get_streetnames("8660")
+print(zipcodes)
+"""
+for zipcode in zipcodes[0:1]:
+    streetnames = get_streetnames(zipcode)
+    for streetname in streetnames[1:2]:
+        print(zipcode, streetname)
+        url = 'https://www.boliga.dk/salg/resultater?so=1&sort=omregnings_dato-d&maxsaledate=today&iPostnr={0}&gade={1}&type=&minsaledate=1992'.format(zipcode, streetname)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        sales = soup.find_all('td', class_="align-middle text-center text-md-left d-md-table-cell")
+        for sale in sales:
+            address = sale.contents[1].contents[1].contents[0]
+            price = int(sale.contents[3].contents[1].contents[1].contents[0].replace(' kr.', '').replace('.', ''))
+            date = sale.contents[3].contents[3].contents[0].split(', ')[0]
+            sale_type = sale.contents[3].contents[3].contents[0].split(', ')[1]
+            house_type = sale.contents[3].contents[5].contents[0].split(', ')[0]
+            house_area = sale.contents[3].contents[5].contents[0].split(', ')[1].replace(' m', '')
+
+            print(address, price, date, sale_type, house_type, house_area, sep='\n')
+"""
