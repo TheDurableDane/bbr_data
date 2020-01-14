c97960a562c7c150c515a0f69a27425cc8cae606
Thomas <thomaslolkschmidt@gmail.com>
2020-01-14 21:20:07 +0100

    Added storage of data.

--- src/scrape_house_prices.py
+++ src/scrape_house_prices.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
 import requests
 import time
 import pandas as pd
+import database_functions as dbf
 
-"""
-house_price_columns = ['address',
+
+def scrape_data():
+    house_price_columns = ['address',
                        'zip_code',
                        'price',
                        'sold_date',
@@ -19,8 +22,8 @@ house_price_columns = ['address',
                        'municipality_code',
                        'estate_code',
                        'city']
-"""
-columns_to_use = ['address',
+
+    columns_to_use = ['address',
                   'zipCode',
                   'price',
                   'soldDate',
@@ -36,16 +39,31 @@ columns_to_use = ['address',
                   'municipalityCode',
                   'estateCode',
                   'city']
-house_price_data = pd.DataFrame(columns=columns_to_use)
+    house_price_data = pd.DataFrame(columns=columns_to_use)
 
-n_subpages = 2
-for subpage_number in range(n_subpages):
-    url = 'https://api.boliga.dk/api/v2/sold/search/results?page={}&sort=date-d'.format(subpage_number + 1)
-    r = requests.get(url)
-    json_object = r.json()
-    temp_df = pd.DataFrame(json_object['results'])
-    temp_df_subset = temp_df[columns_to_use]
-    
-    house_price_data = house_price_data.append(temp_df, ignore_index=True, sort=False)
+    n_subpages = 27508
+    for subpage_number in range(n_subpages):
+        url = 'https://api.boliga.dk/api/v2/sold/search/results?page={}&sort=date-d'.format(subpage_number + 1)
+        r = requests.get(url)
+        json_object = r.json()
+        temp_df = pd.DataFrame(json_object['results'])
+        temp_df_subset = temp_df[columns_to_use]
+        house_price_data = house_price_data.append(temp_df_subset, ignore_index=True, sort=False)
+        time.sleep(1)
 
-    time.sleep(1)
+    house_price_data.columns = house_price_columns
+
+    return house_price_data
+
+
+def store_data_in_database(database_path, data):
+    table_name = 'house_price_data'
+    query = '''INSERT OR IGNORE INTO {} (address, zip_code, price, sold_date, property_type, sale_type, rooms, residence_size, build_year, price_change, guid, latitude, longitude, municipality_code, estate_code, city)
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'''.format(table_name)
+    dbf.insert_into_database(database_path, query, data)
+
+
+if __name__ == '__main__':
+    database_path = '../data/bbr.db'
+    data = scrape_data()
+    store_data_in_database(database_path, data)
