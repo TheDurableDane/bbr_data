59268c26466228906cfcf8436478face5583a1c1
Thomas <thomaslolkschmidt@gmail.com>
2020-01-29 20:12:39 +0100

    Added histograms.

--- src/app.py
+++ src/app.py
@@ -3,21 +3,30 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import database_functions as dbf
 
+
+data = dbf.read_house_price_data('../data/bbr.db')
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 
 tab_histograms = dbc.Row([
-    dbc.Col(width={"size": 2, "offset": 3}, children=[
-        dbc.DropdownMenu(label='ddm', children=[
-            dbc.DropdownMenuItem('item1'),
-            dbc.DropdownMenuItem('item2'),
-            dbc.DropdownMenuItem('item3')
-        ])
+    dbc.Col(width={"size": 2}, children=[
+        dcc.Dropdown(id='hist_data',
+            options=[
+                {'label': 'Price', 'value': 'price'},
+                {'label': 'Rooms', 'value': 'rooms'},
+                {'label': 'Residence size', 'value': 'residence_size'}
+            ],
+            value='price',
+            clearable=False
+        )
     ]),
-    dbc.Col(width=4, children=[
-        html.P('Graphs and content.')
+    dbc.Col(width=10, children=[
+        dcc.Graph(id='histogram')
     ])
 ])
 
@@ -59,6 +68,15 @@ app.layout = dbc.Row([
         ])
     ])
 ])
+
+
+@app.callback(
+    Output(component_id='histogram', component_property='figure'),
+    [Input(component_id='hist_data', component_property='value')])
+def update_histogram(histogram_dropdown_value):
+    fig = px.histogram(data, x=histogram_dropdown_value)
+
+    return fig
 
 
 if __name__ == '__main__':
--- src/database_functions.py
+++ src/database_functions.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 
 def connect_to_database(path):
@@ -23,3 +24,21 @@ def insert_into_database(database_path, query, data):
     connection, cursor = connect_to_database(database_path)
     cursor.executemany(query, data.values)
     disconnect_from_database(connection)
+
+
+def select_from_database(database_path, query):
+    connection, cursor = connect_to_database(database_path)
+    cursor.execute(query)
+    data = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    disconnect_from_database(connection)
+    data = pd.DataFrame(data=data, columns=column_names)
+
+    return data
+
+
+def read_house_price_data(database_path):
+    query = 'SELECT * FROM house_price_data;'
+    house_price_data = select_from_database(database_path, query)
+
+    return house_price_data
